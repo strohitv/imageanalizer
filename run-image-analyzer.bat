279477b4a2bfd045f5dff98bbf4a772bda0c75ce
marco <goethe@marco-fuchs.de>
2022-05-15 10:12:53 +0200

    make jar runnable again and refresh outdated start bat

--- run-image-analyzer.bat
+++ run-image-analyzer.bat
@@ -1,1 +1,2 @@
-mvn spring-boot:run
+call mvn -U clean package
+call java -jar target\imageanalyzer-0.0.1-SNAPSHOT-jar-with-dependencies.jar
--- src/main/java/tv/strohi/twitch/imageanalyzer/ImageanalyzerApplication.java
+++ src/main/java/tv/strohi/twitch/imageanalyzer/ImageanalyzerApplication.java
@@ -3,9 +3,7 @@ package tv.strohi.twitch.imageanalyzer;
 import tv.strohi.twitch.imageanalyzer.analyzer.Analyzer;
 
 public class ImageanalyzerApplication {
-
 	public static void main(String[] args) {
 		new Analyzer();
 	}
-
 }
